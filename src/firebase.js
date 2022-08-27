7637b18117f44afb78d859748d9aad77501a9bf8
Nasrat Nejat <85827553+nasratnejat@users.noreply.github.com>
2022-08-27 09:55:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,21 +7,7 @@ import { getStorage } from 'firebase/storage'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyDGHtIw6vYjYUfAa3xilMQVqps_LyDTGgA',
 
-	authDomain: 'slack-clone-d5e0d.firebaseapp.com',
-
-	databaseURL: 'https://slack-clone-d5e0d-default-rtdb.firebaseio.com',
-
-	projectId: 'slack-clone-d5e0d',
-
-	storageBucket: 'slack-clone-d5e0d.appspot.com',
-
-	messagingSenderId: '722482103299',
-
-	appId: '1:722482103299:web:554a28810f8230df23ba36',
-
-	measurementId: 'G-XKY1E1L3PP',
 }
 
 const app = firebase.initializeApp(firebaseConfig)
